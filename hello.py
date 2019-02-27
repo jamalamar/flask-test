394790b8feff4e930e91c009667d5b03d5a18b37
jamalamar <jamal_ab22@hotmail.com>
2019-02-27 16:46:24 -0600

    first commit

---
+++ hello.py
@@ -0,0 +1,60 @@
+from flask import Flask, url_for
+from flask import request
+from flask import render_template
+app = Flask(__name__)
+
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+#     # show the user profile for that user
+#     return 'User %s' % username
+
+# @app.route('/post/<int:post_id>')
+# def show_post(post_id):
+#     # show the post with the given id, the id is an integer
+#     return 'Post %d' % post_id
+
+# @app.route('/path/<path:subpath>')
+# def show_subpath(subpath):
+#     # show the subpath after /path/
+#     return 'Subpath %s' % subpath
+
+
+
+
+@app.route('/')
+def index():
+    return 'index'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(username)
+
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='Jamal Amar'))
+    print(url_for('static', filename='style.css'))
+
+
+
+
+
